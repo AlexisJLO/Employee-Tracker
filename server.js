b2fc59fe58fce2f47a07f487aa208b8bb4bf4d85
Alexis Lopez <lopez111alex@gmail.com>
2023-12-07 03:21:05 -0800

    Added startApplication function where it asks the user for wha they want to see and give them options

--- server.js
+++ server.js
@@ -1,11 +1,73 @@
 const express = require("express");
 const mysql = require("mysql2");
+const inquirer = require("inquirer");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "employeeTracker_db",
+  },
+  console.log(`Connected to the employeeTracker_db database.`)
+);
+
+function startApp() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "Menu",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "View All Departments",
+        "Add Department",
+        "Quit",
+      ],
+    })
+    .then((answers) => {
+      switch (answers.Menu) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+
+        case "View All Roles":
+          viewAllRoles();
+          break;
+
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+
+        case "Quit":
+          console.log("Goodbye");
+          process.exit();
+      }
+    });
+}
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+startApp();
