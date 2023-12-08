795124d51bbb2351b88358838e61318f141fd676
Alexis Lopez <lopez111alex@gmail.com>
2023-12-07 21:13:28 -0800

    added auto increment for ids

--- db/schema.sql
+++ db/schema.sql
@@ -4,20 +4,20 @@ CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(100),
-    salary DECIMAL(6, 2),
-    department_id INT.
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(225),
+    salary DECIMAL(10, 2),
+    department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
